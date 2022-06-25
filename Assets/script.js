8f4644b5366ef8e75d5e03e026158569df7063f5
Skyler Ferreira <sky_3489@icloud.com>
2022-06-25 12:32:27 -0700

    js file and styling

---
+++ Assets/script.js
@@ -0,0 +1,20 @@
+var dataEl = document.getElementsByClassName("Data")
+console.log(dataEl);
+
+//Open Weather
+var openWeatherUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+
+// covid api
+var covidUrl = 'https://covid-19-statistics.p.rapidapi.com/regions'
+//   url: 'https://covid-19-statistics.p.rapidapi.com/regions',
+//   headers: {
+//     'X-RapidAPI-Key': '79e3ad0431msh7dd9022f1318b61p1bef5djsne6dd6580b13e',
+//     'X-RapidAPI-Host': 'covid-19-statistics.p.rapidapi.com'
+
+// safe travel
+var safeTravelUrl = 'https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200'
+// 'https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200' -H 'Authorization: Bearer {{token}}'
+//   API Key : 84IdbmU5IAg8CYTmFOo5KnyoGR4P4tR6
+//   API Secret : bNF45tKhpnYppupm
