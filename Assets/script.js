7c9ce13e39a35cc31414bb7b04a08ed2941d9103
chris-one <102882070+chris-one@users.noreply.github.com>
2022-06-30 19:09:25 -0700

    google maps direction

--- Assets/script.js
+++ Assets/script.js
@@ -29,35 +29,24 @@ function getCovidApi() {
  }
  covidFetchBtn.addEventListener('click', getCovidApi)
 // safe travel
-var safeTravelUrl = 'https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200'
-// 'https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200' -H 'Authorization: Bearer {{token}}'
-//   API Key : 84IdbmU5IAg8CYTmFOo5KnyoGR4P4tR6
-//   API Secret : bNF45tKhpnYppupm
+var safeTravelUrl = "https://google-maps28.p.rapidapi.com/maps/api/place/details/json?fields=address_component%2Cadr_address%2Cbusiness_status%2Cformatted_address%2Cgeometry%2Cicon%2Cicon_mask_base_uri%2Cicon_background_color%2Cname%2Cpermanently_closed%2Cphoto%2Cplace_id%2Cplus_code%2Ctype%2Curl%2Cutc_offset%2Cvicinity%2Cformatted_phone_number%2Cinternational_phone_number%2Copening_hours%2Cwebsite%2Cprice_level%2Crating%2Creview%2Cuser_ratings_total&place_id=ChIJ37HL3ry3t4kRv3YLbdhpWXE&language=en&region=en"
 
-// function getTravelApi() {
-//     var safeTravelUrl = 'https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200'
-//     fetch(safeTravelUrl) 
-//         .then(function(responxe) {
-//         return response.json();
-//     })
-//     .then(function(data) {
-//     })
-
-// }
-
+// function getTravelApi()
 function getTravelApi() {
-    const options ={
-        method: 'GET',
-        headers: {
-            'API-Key' : '84IdbmU5IAg8CYTmFOo5KnyoGR4P4tR6',
-            'API-Secret' : 'bNF45tKhpnYppupm'
-        }
-    };
 
-    fetch('https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+var safeTravelBtn = document.getElementById("travel")
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2091fc13f6msh05604bf0f21c13dp1243e5jsn71a08e6cf84d',
+		'X-RapidAPI-Host': 'google-maps28.p.rapidapi.com'
+	}
+};
+
+fetch('https://google-maps28.p.rapidapi.com/maps/api/place/details/json?fields=address_component%2Cadr_address%2Cbusiness_status%2Cformatted_address%2Cgeometry%2Cicon%2Cicon_mask_base_uri%2Cicon_background_color%2Cname%2Cpermanently_closed%2Cphoto%2Cplace_id%2Cplus_code%2Ctype%2Curl%2Cutc_offset%2Cvicinity%2Cformatted_phone_number%2Cinternational_phone_number%2Copening_hours%2Cwebsite%2Cprice_level%2Crating%2Creview%2Cuser_ratings_total&place_id=ChIJ37HL3ry3t4kRv3YLbdhpWXE&language=en&region=en', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 }
 
 safeTravelBtn.addEventListener('click',getTravelApi);
